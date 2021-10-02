63c03cc070b02568a98d346a3999adfaf7ab1284
Nikunj Malani <nikunjmalani.031020@gmail.com>
2021-10-02 16:23:00 +0530

    Complete API

--- bananaspeak.js
+++ bananaspeak.js
@@ -1,10 +1,23 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv=document.querySelector("#output");
-btnTranslate.addEventListener("click", clickEventHandler)
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output")
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+btnTranslate.addEventListener("click", clickeventhandler)
 
-
-function clickEventHandler() {
-   outputDiv.innertext= "isjrfiurfbnifun" + txtInput.value;
+function errorHandler(error){
+    console.log("Error Occured", error)
+    alert("Sorry Please Try After Some Time")
+}
    
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function clickeventhandler() {
+    var inputText = txtInput.value;
+    
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => console.log(json.contents.translated))
+        .catch()
 }
