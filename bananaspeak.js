ead3057eb42861d615288ef057cb43ed7c0aed6a
Nikunj Malani <nikunjmalani.031020@gmail.com>
2021-10-04 19:14:52 +0530

    Program Changes

--- bananaspeak.js
+++ bananaspeak.js
@@ -3,11 +3,6 @@ var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 btnTranslate.addEventListener("click", clickeventhandler)
-
-function errorHandler(error){
-    console.log("Error Occured", error)
-    alert("Sorry Please Try After Some Time")
-}
    
 function getTranslationURL(text) {
     return serverURL + "?" + "text=" + text
@@ -24,4 +19,7 @@ function clickeventhandler() {
         .catch(errorHandler)
 }
 
-
+function errorHandler(error){
+    console.log("Error Occured", error)
+    alert("Sorry Please Try After Some Time")
+}
