bdbddcf360a1cb54c00d52e2f47f412770a6db69
Nikunj Malani <nikunjmalani.031020@gmail.com>
2021-10-06 18:35:16 +0530

    Code Placement Changes

--- bananaspeak.js
+++ bananaspeak.js
@@ -3,23 +3,25 @@ var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 btnTranslate.addEventListener("click", clickeventhandler)
-   
+
 function getTranslationURL(text) {
     return serverURL + "?" + "text=" + text
 }
 
+function errorHandler(error) {
+    console.log("Error Occured", error)
+    alert("Sorry Please Try After Some Time")
+}
+
+
 function clickeventhandler() {
     var inputText = txtInput.value;
-    
+
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText})
+            outputDiv.innerText = translatedText
+        })
         .catch(errorHandler)
-}
-
-function errorHandler(error){
-    console.log("Error Occured", error)
-    alert("Sorry Please Try After Some Time")
 }
